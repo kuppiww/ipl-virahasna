0cf4e127ad45d9ae9b0925a95f447cc5a02d72c9
VIRAA\virah_19w0wcb <virahasnaf02@gmail.com>
2023-10-23 11:28:36 +0700

    perubahan sesuai prinsip liskov subs

--- lsp/Product.java
+++ lsp/Product.java
@@ -1,10 +1,9 @@
 package lsp;
 
+import java.util.Date;
+
 public class Product {
     abstract String getName();
-    abstract Date getExpiredDate();
-
     public void getProducInfo(){
-
     }
 }
--- lsp/Smartphone.java
+++ lsp/Smartphone.java
@@ -1,12 +1,10 @@
 package lsp;
 
+
 public class Smartphone extends Product{
     @Override
     String getName(){
         return "Samsung S22 Ultra"
     }
-    @Override
-    Date getExpiredDate(){
-        return new Date(0);
-    }
+    
 }
